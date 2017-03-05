424c2a7a42d25190f7abb24721d9c179e8a3f31e
fervi <thunderstormpolska@o2.pl>
2017-03-05 21:05:53 +0100

    Add 1st version of card level documentation.

--- cardgenerator/data.lua
+++ cardgenerator/data.lua
@@ -1,10 +1,10 @@
-bot = true;
+bot = false;
 
 text = "Swords of Revealing Light";
 stars = 8;
 -- 1 - monster, 2 - spell, 3 - trap
 cardtype = 3;
-background = "spellcards";
+background = "ritualcards";
 eftext = "blablabla";
 
 --type:
