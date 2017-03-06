5797ae9b4f055cfe921f6b4917d87112beb1913b
fervi <thunderstormpolska@o2.pl>
2017-03-06 01:58:22 +0100

    Deleted backgrounds (files), replaced by color function in Love2D

--- cardgenerator/main.lua
+++ cardgenerator/main.lua
@@ -10,29 +10,59 @@ fontcardname = love.graphics.newFont("fonts/lmmonocaps10-regular.otf", 25)
 fontcardnamesmall = love.graphics.newFont("fonts/lmmonocaps10-regular.otf", 22)
 fontcardtextbig = love.graphics.newFont("fonts/GoMonoRegular.ttf", 20)
 fontcardtext = love.graphics.newFont("fonts/GoMonoRegular.ttf", 12)
-gfx_bg = love.graphics.newImage('backgrounds/' .. background .. '.png')
 gfx_star = love.graphics.newImage("sprites/star.png");
 gfx_card = love.graphics.newImage("sprites/cardgraph.png");
 
 function love.draw()
-love.graphics.setColor( 255, 255, 255)
+love.graphics.setColor(255, 255, 255)
 love.graphics.rectangle("fill", 0, 0, 354, 508)
 
 -- border
-love.graphics.setColor( 0, 0, 0)
+love.graphics.setColor(0, 0, 0)
 love.graphics.rectangle("line", 0, 0, 354, 508)
-love.graphics.setColor( 20, 20, 20)
+love.graphics.setColor(20, 20, 20)
 love.graphics.rectangle("line", 1, 1, 353, 507)
-love.graphics.setColor( 40, 40, 40)
+love.graphics.setColor(40, 40, 40)
 love.graphics.rectangle("line", 2, 2, 352, 506)
-love.graphics.setColor( 60, 60, 60)
+love.graphics.setColor(60, 60, 60)
 love.graphics.rectangle("line", 3, 3, 351, 505)
-love.graphics.setColor( 80, 80, 80)
+love.graphics.setColor(80, 80, 80)
 love.graphics.rectangle("line", 4, 4, 350, 504)
 
 -- add card background
-love.graphics.draw(gfx_bg, 5, 5, 0, 0.435, 0.836, 0, 0)
-love.graphics.setColor( 255, 255, 255)
+
+if(cardtype==1)
+then
+love.graphics.setColor(185, 165, 75)
+end
+
+if(cardtype==2)
+then
+love.graphics.setColor(15, 129, 130)
+end
+
+if(cardtype==3)
+then
+love.graphics.setColor(122, 67, 96)
+end
+
+if(cardtype==4)
+then
+love.graphics.setColor(191, 116, 76)
+end
+
+if(cardtype==5)
+then
+love.graphics.setColor(135, 78, 159)
+end
+
+if(cardtype==6)
+then
+love.graphics.setColor(94, 110, 169)
+end
+
+love.graphics.rectangle("fill", 5, 5, 999, 999)
+love.graphics.setColor(255, 255, 255)
 
 -- add card name
 love.graphics.setFont(fontcardname)
@@ -50,7 +80,7 @@ end
 
 
 -- add stars
-if(cardtype==1)
+if((cardtype==1 or cardtype==4) or (cardtype==5 or cardtype==6))
 then
 love.graphics.setFont(fontcardtextbig)
 love.graphics.draw(gfx_star, 308, 45, 0, 1, 1, 0, 0);
