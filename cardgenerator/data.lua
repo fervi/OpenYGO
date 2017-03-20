33854354d5eca2ae7f232ef42b9e5611f7b2a266
fervi <thunderstormpolska@o2.pl>
2017-03-20 20:06:47 +0100

    Added backgrounds and 2 monsters graphs (Supertux and Replicant)

--- cardgenerator/data.lua
+++ cardgenerator/data.lua
@@ -4,7 +4,7 @@ text = "Freedom Hero: Supertux";
 stars = 4;
 -- 1 - monster, 2 - spell, 3 - trap, 4 - effect, 5 - fusion, 6 - ritual
 cardtype = 4;
-eftext = "If this is the only card in your hand, you can Special Summon it.\nWhen this card is Summoned, if you control no other cards: you can draw 2 cards.";
+eftext = "If this is the only card in your hand, you can Special Summon it. \n When this card is Summoned, if you control no other cards: you can draw 2 cards.";
 atk = 800;
 def = 1200;
 -- 0 - traps and spells, 1 - warrior, 2 - spellcaster, 4 - fairy, 8 - fiend, 16 - zombie, 32 - machine, 64 - aqua, 128 - pyro, 256 - rock, 512 - winged beast, 1024 - plant, 2048 - insect, 4096 - Thunder, 8192 - Dragon, 16384 - Beast, 32768 - Beast Warrior, 65536 - Dinosaur, 131072 - Fish, 262144 - Sea Serpent, 524288 - Reptile, 1048576 - Psychic, 2097152 - Divine Beast, 4194304 - Creator God, 8388608 - Wyrm
