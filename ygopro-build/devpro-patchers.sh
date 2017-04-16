0bc80510c82f9517d1e65f95d2a719efe748d35c
fervi <thunderstormpolska@o2.pl>
2017-04-16 23:42:20 +0200

    Fixed issue with deleting YGOPro Devpro client

--- ygopro-build/devpro-patchers.sh
+++ ygopro-build/devpro-patchers.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-wget http://www.ygodev.pro/launcher/installers/YGOPro%20DevPro.exe
-unar YGOPro\ DevPro.exe
-rm YGOPro\ DevPro.exe
-cd YGOPro\ DevPro
+cd DevPro
 cp strings.conf ../binaries/strings.conf
 cp cards.cdb ../binaries/cards.cdb
