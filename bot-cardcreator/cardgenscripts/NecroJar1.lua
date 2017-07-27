0e1611f7c2b8ddd64588ec270804c0edb20c8fe3
fervi <thunderstormpolska@o2.pl>
2017-07-27 22:03:17 +0200

    Add 3 cards - Necro Jar (#1, #2, #3)

---
+++ bot-cardcreator/cardgenscripts/NecroJar1.lua
@@ -0,0 +1,11 @@
+text = "Necro Jar #1";
+eftext = "FLIP: Both players throw all cards from their hands, then draw 5 cards.";
+
+id=1005;
+alias=0;
+type=2097185;
+atk=700;
+def=300;
+level=3;
+race=8;
+attribute=32;
---
+++ bot-cardcreator/cardgenscripts/NecroJar2.lua
@@ -0,0 +1,11 @@
+text = "Necro Jar #2";
+eftext = "FLIP: All monsters from the field go to the decks, the deck will be shuffled. Then the players draw until they get the same number of monsters that go into deck. If the monster has a level 4 or below it is special summoned, the rest of the card goes to the graveyard."
+
+id=1006;
+alias=0;
+type=2097185;
+atk=800;
+def=200;
+level=3;
+race=8;
+attribute=32;
