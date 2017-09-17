6b22712d24b661d07fe2db0827b2aeede27c32bb
fervi <thunderstormpolska@o2.pl>
2017-09-17 19:28:15 +0200

    Added "Rise from your grave" card

---
+++ bot-cardcreator/cardgenscripts/RiseFromYourGrave.lua
@@ -0,0 +1,11 @@
+text = "Rise From Your Grave";
+eftext = "Special summon the monster from your graveyard in attack mode. If this card is destroyed, destroy the summoned monster. If the monster is destroyed - destroy this card.";
+
+id=1008;
+alias=0;
+type=131076;
+atk=0;
+def=0;
+level=0;
+race=0;
+attribute=0;
