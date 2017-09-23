3454a5b561bf9331ab4fffaffdb0ee1aebe9345b
fervi <thunderstormpolska@o2.pl>
2017-09-23 21:32:14 +0200

    Added "Cyber Archer" Card

---
+++ bot-cardcreator/cardgenscripts/CyberArcher.lua
@@ -0,0 +1,11 @@
+text = "Cyber Archer";
+eftext = "If your opponent has monsters in the field and you do not - you can special summon this monster.";
+
+id=1009;
+alias=0;
+type=33;
+level=5;
+atk=1800;
+def=800;
+race=32;
+attribute=16;
