a34a793df7c70f3563b1c2b44f1f4560a0471bf1
fervi <thunderstormpolska@o2.pl>
2017-09-24 20:54:42 +0200

    Added "Linux Ranger - Green" card

---
+++ bot-cardcreator/cardgenscripts/GreenLinuxRanger.lua
@@ -0,0 +1,11 @@
+text = "Linux Ranger - Green";
+eftext = "Everyone has the potential to win with the stronger.";
+
+id=1010;
+alias=0;
+type=17;
+level=4;
+atk=1500;
+def=1200;
+race=64;
+attribute=2;
