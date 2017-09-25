fca92557811a21a13d011f65b149dd3589705be2
fervi <thunderstormpolska@o2.pl>
2017-09-25 19:16:05 +0200

    Added "Linux Ranger - Blue" card

---
+++ bot-cardcreator/cardgenscripts/BlueLinuxRanger.lua
@@ -0,0 +1,11 @@
+text = "Linux Ranger - Blue";
+eftext = "You can discard one card during the Main Phase, during Battle Phase you can attack twice.";
+
+id=1011;
+alias=0;
+type=33;
+level=4;
+atk=1100;
+def=450;
+race=512;
+attribute=2;
