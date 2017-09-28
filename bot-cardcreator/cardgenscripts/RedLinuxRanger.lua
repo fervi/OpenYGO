dce9da6dbaa4fa532eda6d3a4318c1bbaec5596f
fervi <thunderstormpolska@o2.pl>
2017-09-28 10:00:51 +0200

    Added "Linux Ranger - Red" card.

---
+++ bot-cardcreator/cardgenscripts/RedLinuxRanger.lua
@@ -0,0 +1,11 @@
+text = "Linux Ranger - Red";
+eftext = "Defending penguins from predators is their most important task.";
+
+id=1012;
+alias=0;
+type=17;
+level=4;
+atk=1000;
+def=1800;
+race=64;
+attribute=2;
