5455c0c4299c977520dbc38aef622df7aa41c72a
fervi <thunderstormpolska@o2.pl>
2017-10-10 21:54:09 +0200

    Added "Linux Ranger - Pink" card.

---
+++ bot-cardcreator/cardgenscripts/PinkLinuxRanger.lua
@@ -0,0 +1,12 @@
+text = "Linux Ranger - Pink";
+eftext = "This card can attack the opponent directly.";
+
+id=1016;
+alias=0;
+type=33;
+level=3;
+atk=700;
+def=500;
+race=512;
+attribute=2;
+limit=3;
