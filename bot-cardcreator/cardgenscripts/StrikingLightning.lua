8297e8931443d75ede0d2839bd1b68c40d687771
fervi <thunderstormpolska@o2.pl>
2017-11-15 23:15:21 +0100

    Added "Striking Lightning" card.

---
+++ bot-cardcreator/cardgenscripts/StrikingLightning.lua
@@ -0,0 +1,12 @@
+text = "Striking Lightning";
+eftext = "Discard 1 card. Select 1 Trap / Spell card on the field - Destroy it.";
+
+id=1017;
+alias=0;
+type=2;
+atk=0;
+def=0;
+level=0;
+race=0;
+attribute=0;
+limit=2;
