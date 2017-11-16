d04e3edf854853e468eaf77ba78dcd3ddb8b7f71
fervi <thunderstormpolska@o2.pl>
2017-11-16 21:35:41 +0100

    Added "Linux Ranger - Yellow" card.

---
+++ bot-cardcreator/cardgenscripts/YellowLinuxRanger.lua
@@ -0,0 +1,12 @@
+text = "Linux Ranger - Yellow";
+eftext = "If this card attacks, it goes into defense position.";
+
+id=1018;
+alias=0;
+type=33;
+level=4;
+atk=1800;
+def=300;
+race=512;
+attribute=2;
+limit=3;
