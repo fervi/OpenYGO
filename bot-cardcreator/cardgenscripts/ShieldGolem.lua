4eab707dfe416c5a9c19960d1b8abbe6595070a9
fervi <thunderstormpolska@o2.pl>
2017-12-04 21:42:59 +0100

    Added "Shield Golem" card.

---
+++ bot-cardcreator/cardgenscripts/ShieldGolem.lua
@@ -0,0 +1,12 @@
+text = "Shield Golem";
+eftext = "The ultimate defense straight from the depths of the earth.";
+
+id=1021;
+alias=0;
+type=17;
+level=4;
+atk=800;
+def=2000;
+race=256;
+attribute=1;
+limit=3;
