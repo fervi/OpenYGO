9496589df4da03f14cc93e20758028e5abaf6dd5
fervi <thunderstormpolska@o2.pl>
2017-12-05 15:47:44 +0100

    Added "Assault Golem" card.

---
+++ bot-cardcreator/cardgenscripts/AssaultGolem.lua
@@ -0,0 +1,12 @@
+text = "Assault Golem";
+eftext = "Shield Golem + Sword Golem.";
+
+id=1024;
+alias=0;
+type=65;
+level=7;
+atk=2400;
+def=2000;
+race=256;
+attribute=1;
+limit=3;
---
+++ bot-cardcreator/scripts/AssaultGolem.lua
@@ -0,0 +1,5 @@
+function c1024.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcCode2(c,1021,1022,true,true)
+end
