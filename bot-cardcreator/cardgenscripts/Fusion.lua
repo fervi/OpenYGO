75e981c02c06eb4e16b946111f6b3f43746a87ee
fervi <thunderstormpolska@o2.pl>
2017-12-05 14:54:59 +0100

    Added "Fusion" card.

---
+++ bot-cardcreator/cardgenscripts/Fusion.lua
@@ -0,0 +1,12 @@
+text = "Fusion";
+eftext = "Fusion summon 1 monster from Extra Deck, using monsters from your hand or your side of the field as Fusion Materials.";
+
+id=1023;
+alias=0;
+type=2;
+atk=0;
+def=0;
+level=0;
+race=0;
+attribute=0;
+limit=3;
