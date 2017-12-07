8ab741449d4976ab69669f4ee709b33ce5ce6379
fervi <thunderstormpolska@o2.pl>
2017-12-07 23:53:28 +0100

    Added "Block Defense" card.

---
+++ bot-cardcreator/cardgenscripts/BlockDefense.lua
@@ -0,0 +1,12 @@
+text = "Block Defense";
+eftext = "Select 1 opponents monster on the field in Defense Position - Change it to Attack position";
+
+id=1027;
+alias=0;
+type=2;
+atk=0;
+def=0;
+level=0;
+race=0;
+attribute=0;
+limit=3;
