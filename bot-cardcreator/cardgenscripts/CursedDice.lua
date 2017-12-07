2e978ad9e0fbe8424621cd9a97d81333529afe0e
fervi <thunderstormpolska@o2.pl>
2017-12-07 12:43:31 +0100

    Added "Cursed Dice" card.

---
+++ bot-cardcreator/cardgenscripts/CursedDice.lua
@@ -0,0 +1,12 @@
+text = "Cursed Dice";
+eftext = "Roll a dice. All opponent monsters lose ATK and DEF equal to the result * 100, until the End Phase.";
+
+id=1025;
+alias=0;
+type=4;
+atk=0;
+def=0;
+level=0;
+race=0;
+attribute=0;
+limit=3;
