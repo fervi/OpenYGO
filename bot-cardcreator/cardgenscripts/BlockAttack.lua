86eea0c531ac33ad32b729136232894243956b19
fervi <thunderstormpolska@o2.pl>
2017-12-07 14:44:57 +0100

    Added "Block Attack" card.

---
+++ bot-cardcreator/cardgenscripts/BlockAttack.lua
@@ -0,0 +1,12 @@
+text = "Block Attack";
+eftext = "Select 1 opponents face-up monster on the field in Attack Position - Change it to Defense position";
+
+id=1026;
+alias=0;
+type=2;
+atk=0;
+def=0;
+level=0;
+race=0;
+attribute=0;
+limit=3;
---
+++ bot-cardcreator/scripts/BlockAttack.lua
@@ -0,0 +1,27 @@
+function c1026.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c1026.target)
+	e1:SetOperation(c1026.activate)
+	c:RegisterEffect(e1)
+end
+function c1026.filter(c)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition()
+end
+function c1026.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c1026.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c1026.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,c1026.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+end
+function c1026.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsPosition(POS_FACEUP_ATTACK) then
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
+	end
+end
