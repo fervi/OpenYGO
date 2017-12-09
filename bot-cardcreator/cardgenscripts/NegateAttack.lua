137f73ac73e58e745937c37bf0bcff3692823dcf
fervi <thunderstormpolska@o2.pl>
2017-12-09 01:50:04 +0100

    Added "Negate Attack" card.

---
+++ bot-cardcreator/cardgenscripts/NegateAttack.lua
@@ -0,0 +1,12 @@
+text = "Negate Attack";
+eftext = "When an opponents monster declare attack - negate attack and end the Battle Phase.";
+
+id=1028;
+alias=0;
+type=1048580;
+atk=0;
+def=0;
+level=0;
+race=0;
+attribute=0;
+limit=3;
