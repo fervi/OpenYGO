8719e9902ffe8d564bba629eee095312139f5c69
fervi <thunderstormpolska@o2.pl>
2017-12-10 16:32:27 +0100

    Added "Cyber Lancer" card.

---
+++ bot-cardcreator/cardgenscripts/CyberLancer.lua
@@ -0,0 +1,12 @@
+text = "Cyber Lancer";
+eftext = "If you attack a monster in a defense position and its defense points are smaller than your attack points - subtract the difference between them from your opponents Life Points. If this card attacked, it will go to the defense position after the battle phase.";
+
+id=1029;
+alias=0;
+type=33;
+level=8;
+atk=2400;
+def=1600;
+race=32;
+attribute=16;
+limit=3;
