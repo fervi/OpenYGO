c451716ce72122cffa53fad6514635d1a3c51c13
fervi <thunderstormpolska@o2.pl>
2017-12-13 20:30:22 +0100

    Added "Cyber Saber" card.

---
+++ bot-cardcreator/cardgenscripts/CyberSaber.lua
@@ -0,0 +1,12 @@
+text = "Cyber Saber";
+eftext = "The fight is inscribed in the name of the Hero.";
+
+id=1030;
+alias=0;
+type=17;
+level=6;
+atk=2100;
+def=1300;
+race=32;
+attribute=16;
+limit=3;
