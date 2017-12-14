0559d4906525ed10a41a5a56e72b0a2f8674bcd7
fervi <thunderstormpolska@o2.pl>
2017-12-14 14:34:52 +0100

    Added "Spell Mine" card.

---
+++ bot-cardcreator/cardgenscripts/SpellMine.lua
@@ -0,0 +1,12 @@
+text = "Spell mine";
+eftext = "Activate when a spell card is activated, discard card; negate activation and destroy this spell.";
+
+id=1031;
+alias=0;
+type=1048580;
+level=0;
+atk=0;
+def=0;
+race=0;
+attribute=0;
+limit=3;
