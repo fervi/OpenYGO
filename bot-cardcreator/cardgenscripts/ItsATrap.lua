e2c295e2ebf35feffdad85e36546537969e7ed7a
fervi <thunderstormpolska@o2.pl>
2017-12-14 18:30:32 +0100

    Added "Its a Trap!" card

---
+++ bot-cardcreator/cardgenscripts/ItsATrap.lua
@@ -0,0 +1,12 @@
+text = "It is a Trap!";
+eftext = "Activate when a trap card is activated, negate activation and destroy this trap.";
+
+id=1032;
+alias=0;
+type=1048580;
+level=0;
+atk=0;
+def=0;
+race=0;
+attribute=0;
+limit=3;
