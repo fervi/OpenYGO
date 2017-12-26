2316c60543c69ed5efa8d1eb702ee381c8084b23
fervi <thunderstormpolska@o2.pl>
2017-12-26 20:08:48 +0100

    Added "Crystal Serpent Armor" card.

---
+++ bot-cardcreator/cardgenscripts/CrystalSerpentArmor.lua
@@ -0,0 +1,12 @@
+text = "Crystal Serpent Armor";
+eftext = "A special type of skin allows the serpents to achieve a high level of defense.";
+
+id=1034;
+alias=0;
+type=17;
+level=4;
+atk=900;
+def=1700;
+race=2048;
+attribute=8;
+limit=3;
