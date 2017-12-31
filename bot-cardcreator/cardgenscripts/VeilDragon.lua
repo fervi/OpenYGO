a909e107cba08f70cde3b1053fb101d8d5996ff9
fervi <thunderstormpolska@o2.pl>
2017-12-31 22:26:00 +0100

    Added "Veil Dragon" card.

---
+++ bot-cardcreator/cardgenscripts/VeilDragon.lua
@@ -0,0 +1,12 @@
+text = "Veil Dragon";
+eftext = "FLIP: Select 1 spell card from your graveyard, add it to the hand.";
+
+id=1035;
+alias=0;
+type=33;
+level=4;
+atk=1100;
+def=700;
+race=8192;
+attribute=16;
+limit=3;
