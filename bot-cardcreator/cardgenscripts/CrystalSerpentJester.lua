19834571b73f8a49a0bc197408226f300cf376c1
fervi <thunderstormpolska@o2.pl>
2018-03-21 20:53:13 +0100

    Added "Crystal Serpent Jester" card.

---
+++ bot-cardcreator/cardgenscripts/CrystalSerpentJester.lua
@@ -0,0 +1,12 @@
+text = "Crystal Serpent Jester";
+eftext = "Once per turn this card can not be destroyed.";
+
+id=1036;
+alias=0;
+type=33;
+level=2;
+atk=0;
+def=0;
+race=2048;
+attribute=4;
+limit=3;
