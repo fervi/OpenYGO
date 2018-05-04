3b03f13b245246f8f38b0b305c109fed1d82ffcf
fervi <thunderstormpolska@o2.pl>
2018-05-04 14:12:16 +0200

    Added "Crystal Serpent Blazer" card.

---
+++ bot-cardcreator/cardgenscripts/CrystalSerpentBlazer.lua
@@ -0,0 +1,12 @@
+text = "Crystal Serpent Blazer";
+eftext = "Blazers highly poisonous teeth can kill a strong monster.";
+
+id=1037;
+alias=0;
+type=17;
+level=4;
+atk=1500;
+def=400;
+race=2048;
+attribute=8;
+limit=3;
